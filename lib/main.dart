8c8cbca51ac9df25d83ba5f31d23ddf52f863e06
Yoshi <yoskomu.33@gmail.com>
2021-11-21 16:10:04 +0900

    完成版

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,17 @@ class WelcomPage extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [],
+            children: [
+              _Header(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: _SignInForm(),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 64),
+                child: _Footer(),
+              ),
+            ],
           ),
         ),
       ),
@@ -117,6 +127,234 @@ class _HeaderCircles extends StatelessWidget {
         width: double.infinity,
         height: height,
       ),
+    );
+  }
+}
+
+class _HeaderTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'welcome',
+          style: Theme.of(context).textTheme.headline4!.copyWith(
+                color: kTextColorPrimary,
+                fontWeight: FontWeight.w500,
+              ),
+        ),
+        SizedBox(height: 4),
+        Text(
+          'Sign in to continue',
+          style: Theme.of(context)
+              .textTheme
+              .subtitle2!
+              .copyWith(color: kTextColorPrimary),
+        ),
+      ],
+    );
+  }
+}
+
+class _HeaderBackButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        primary: kButtonTextColorPrimary,
+        backgroundColor: Colors.transparent,
+        shape: CircleBorder(
+          side: BorderSide(color: kButtonColorPrimary),
+        ),
+      ),
+      onPressed: () {},
+      child: Icon(Icons.chevron_left, color: kIconColor),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double height = 320;
+    return Container(
+      height: height,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: _HeaderBackground(height: height),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: _HeaderCircles(height: height),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.only(top: 128),
+              child: _HeaderTitle(),
+            ),
+          ),
+          Positioned(
+            top: 16,
+            left: 0,
+            child: _HeaderBackButton(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomTextField extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final bool obscureText;
+
+  const _CustomTextField({
+    Key? key,
+    required this.labelText,
+    required this.hintText,
+    required this.obscureText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        hintStyle: TextStyle(color: kTextColorSecondary),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: kAccentColor,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: kTextColorSecondary,
+          ),
+        ),
+      ),
+      obscureText: obscureText,
+      onTap: () {},
+    );
+  }
+}
+
+class _SignInForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomTextField(
+          labelText: 'Email',
+          hintText: 'your email address goes here',
+          obscureText: false,
+        ),
+        SizedBox(height: 48),
+        _CustomTextField(
+          labelText: 'Password',
+          hintText: 'your password goes here',
+          obscureText: true,
+        ),
+        SizedBox(height: 4),
+        Text(
+          'Forgot Password?',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2!
+              .copyWith(color: kTextColorSecondary),
+        ),
+        SizedBox(height: 48),
+        Container(
+          width: double.infinity,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              primary: kButtonTextColorPrimary,
+              backgroundColor: kButtonColorPrimary,
+              padding: EdgeInsets.symmetric(vertical: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            onPressed: () {},
+            child: Text(
+              'Sign in',
+              style: Theme.of(context)
+                  .textTheme
+                  .button!
+                  .copyWith(color: kButtonTextColorPrimary, fontSize: 18),
+            ),
+          ),
+        ),
+        SizedBox(height: 16),
+        Text(
+          'OR',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2!
+              .copyWith(color: kTextColorSecondary),
+        ),
+        SizedBox(height: 16),
+        Text(
+          'Connect with',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2!
+              .copyWith(color: kTextColorPrimary),
+        ),
+        SizedBox(height: 24),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {},
+            ),
+            Container(
+              color: kTextColorSecondary,
+              width: 1,
+              height: 16,
+            ),
+            IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {},
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Don\'t have Account?',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2!
+              .copyWith(color: kTextColorSecondary),
+        ),
+        SizedBox(width: 4),
+        Text(
+          'Sign up',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText2!
+              .copyWith(color: kTextColorPrimary),
+        ),
+      ],
     );
   }
 }
